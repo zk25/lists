2e26b6a5cd15c3143de0ecf0c828dd4ff87ecef0
zainab <zk25042003@gmail.com>
2022-11-02 08:01:17 +0000

    Update airdrop.js

--- airdrop.js
+++ airdrop.js
@@ -1,24 +1,22 @@
-npm;
-const { 
-  Connection, 
-  LAMPORTS_PER_SOL, 
-  clusterApiUrl, 
-  Keypair
+const {
+  Connection,
+  LAMPORTS_PER_SOL,
+  clusterApiUrl,
+  Keypair,
 } = require("@solana/web3.js");
 
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-(async ()=> {
-
+(async () => {
   const keypair = Keypair.generate();
 
   const airdropSignature = await connection.requestAirdrop(
     keypair.publicKey,
     LAMPORTS_PER_SOL
   );
-  
+
   const latestBlockHash = await connection.getLatestBlockhash();
-  
+
   const txn = await connection.confirmTransaction({
     blockhash: latestBlockHash.blockhash,
     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
@@ -27,8 +25,8 @@ const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
   console.log({
     publicKey: keypair.publicKey,
-    privateKey: keypair.privateKey,
+    privateKey: keypair.secretKey,
     signature: airdropSignature,
-    txn
-  })
+    txn,
+  });
 })()
